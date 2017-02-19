2c104c7865eab2f28c6bcff48505f56af728c11a
Denis Kiselyov <denis.kiselyov@gmail.com>
2017-02-19 23:47:27 +0300

    add template for rails application

---
+++ template.rb
@@ -0,0 +1,17 @@
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'faker', '>= 1.6.5'
+  gem 'rails-controller-testing'
+end
+
+group :development do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
