4c2bc790ab7dbda6e3815af687d87797e4fbf03a
Denis Kiselyov <denis.kiselyov@gmail.com>
2017-02-19 23:51:50 +0300

    fix template

--- template.rb
+++ template.rb
@@ -1,17 +1,13 @@
-group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+gem 'pg'
+gem 'haml-rails'
+
+gem_group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'faker', '>= 1.6.5'
   gem 'rails-controller-testing'
 end
 
-group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-end
-
-group :test do
+gem_group :test do
   gem 'shoulda-matchers'
 end
